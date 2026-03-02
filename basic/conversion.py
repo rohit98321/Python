20708e79361d00a5960453a760cf76e86fd2fa6f
Rohit Gupta <rohitgupta.dev01@gmail.com>
2026-03-02 21:34:31 +0530

    input and output

---
+++ basic/conversion.py
@@ -0,0 +1,7 @@
+a="12"
+
+a=int(a) #implicit conversion
+print(a)
+
+b=12/4
+print(b) #explicit conversion
---
+++ basic/inputOutput.py
@@ -0,0 +1,11 @@
+name="rohit gupta"
+age=23
+
+
+print("hello world my name is ",name, "and my age is ",age) #raw format
+print(f"hello world my name is {name} and my age is {age}") #format string
+
+
+
+dost=input("what is your name : ") #input function
+print(dost)
